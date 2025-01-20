d6e156b90005f94776825461f4f99e79d45d1578
Eva Emmerich <eva@evaemmerich.com>
2025-01-19 23:18:31 -0600

    ignore missing config file for now

--- sway-regolith.nix
+++ sway-regolith.nix
@@ -22,7 +22,8 @@ in {
       sway-audio-idle-inhibit swaylock swayidle dbus clipman wl-clipboard xwayland
       avizo
       (import ./sway-regolith/default.nix { inherit pkgs; })
-      (import ./regolith-wm-config/default.nix { inherit pkgs; extraConfig = cfg.extraConfig; })
+      # TODO https://github.com/regolith-linux/regolith-wm-config/blob/main/etc/regolith/sway/config  is probably what we want here?
+      #(import ./regolith-wm-config/default.nix { inherit pkgs; extraConfig = cfg.extraConfig; })
       (import ./i3status-rs/default.nix {inherit pkgs;})
       #i3status rs
     ];
